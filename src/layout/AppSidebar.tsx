b1c64b5cef4f9ce60fc5aa16804658c0fa94faf6
Phongsathon Singtui <pongsaton.s@aware.co.th>
2026-04-01 14:13:27 +0700

    wip 🚧 : update bigboss 0.001

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -16,7 +16,7 @@ import {
   UserCircleIcon,
 } from "../icons";
 import { useSidebar } from "../context/SidebarContext";
-import SidebarWidget from "./SidebarWidget";
+// import SidebarWidget from "./SidebarWidget";
 
 type NavItem = {
   name: string;
@@ -368,7 +368,7 @@ const AppSidebar: React.FC = () => {
             </div>
           </div>
         </nav>
-        {isExpanded || isHovered || isMobileOpen ? <SidebarWidget /> : null}
+        {/* {isExpanded || isHovered || isMobileOpen ? <SidebarWidget /> : null} */}
       </div>
     </aside>
   );
