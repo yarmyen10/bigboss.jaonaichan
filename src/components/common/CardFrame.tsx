ca4776e70c54b741a245a031208ea97be3acbf02
yarmyen10 <phongsathon.yen@gmail.com>
2026-04-04 20:08:33 +0700

    wip 🚧 : loading sticker cat

--- src/components/common/CardFrame.tsx
+++ src/components/common/CardFrame.tsx
@@ -10,13 +10,15 @@ export default function CardFrame({ children, isLoading }: Props) {
         <aside>
             {   
                 isLoading ? (
-                    <div className="min-h-screen px-5 py-7 xl:px-10 xl:py-12 animate-pulse">
-                        <div className="mx-auto w-full max-w-[630px] flex items-center justify-center">
+                    <div className="min-h-screen px-5 py-7 xl:px-10 xl:py-12">
+                        
+                        <div className="mx-auto w-full max-w-[630px] flex items-baseline justify-center columns-3xs">
                             <img
                                 className="h-[20%] w-[20%] object-contain"
-                                src="/images/logo/Hungry Cats Sticker by Pusheen.gif"
+                                src="/images/stickers/Hungry Cat Food Sticker by Lord Tofu Animation.gif"
                                 alt="Loading..."
                             />
+                            <p >Loading...</p>
                         </div>
                     </div>
                     // <div className="min-h-screen rounded-2xl border border-gray-200 bg-white px-5 py-7 dark:border-gray-800 dark:bg-white/[0.03] xl:px-10 xl:py-12 animate-pulse">
